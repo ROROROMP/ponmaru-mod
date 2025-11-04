7435ca9414d350aa82b88120bf68fe106f4d06a9
ROROROMP <9ROMP81@gmail.com>
2025-11-04 12:30:03 +0900

    Initial commit for version 1.21.1

---
+++ src/main/java/com/romp/PonmaruModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.romp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PonmaruModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
